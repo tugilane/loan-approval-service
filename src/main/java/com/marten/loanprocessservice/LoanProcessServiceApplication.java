0799780cfcfffd334dcb701a434caa3a1e4407fd
tugilane <marten.peljo@gmail.com>
2026-04-07 23:48:11 +0300

    initialize repo

---
+++ src/main/java/com/marten/loanprocessservice/LoanProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marten.loanprocessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanProcessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marten/loanprocessservice/LoanProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marten.loanprocessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProcessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
